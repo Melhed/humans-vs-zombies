4533fbe5d0ef7e6e6e0f81ea8ade38fe28ae7f06
Melhed <82815800+Melhed@users.noreply.github.com>
2023-03-06 10:25:47 +0100

    Project config & setup

---
+++ backend-hvz/src/main/java/com/example/backendhvz/BackendHvzApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendhvz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHvzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHvzApplication.class, args);
+	}
+
+}
